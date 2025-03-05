bd7a02cf2582cb711750a155a4191998384d6e14
Amin <aminsahrae4@gmail.com>
2025-03-06 02:22:28 +0330

    //

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.{html,js,css}",
-    "./**/*.html"
+    "./index.html",
+    "./**/*.{html,js,css}"
   ],
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-  
-
   theme: {
-    size: {
-      '96': '24rem',
-      '72': '18rem',
-    },
-    
-    extend: {
-        screens: {
-            'sm': '480px',
-           
-        },
-    },
-    fontFamily: {
-        sans: ['Roboto Condensed', 'sans-serif'],
-    },
-    
+    extend: {},
   },
   plugins: [],
 } 
